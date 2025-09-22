22000577ec89de58116d36f6010826c9322ca9df
sanyamarg407 <sanyamarg407@gmail.com>
2025-09-22 08:29:27 -0400

    Initial commit: full final_project folder

---
+++ final_project/EmotionDetection/__init__.py
@@ -0,0 +1,8 @@
+"""
+EmotionDetection package
+exposes the emotion_detector() function.
+"""
+
+from .emotion_detection import emotion_detector
+
+__all__ = ["emotion_detector"]
---
+++ final_project/EmotionDetection/emotion_detection.py
@@ -0,0 +1,33 @@
+# emotion_detection.py
+import requests
+import json
+
+def emotion_detector(text_to_analyze: str):
+    """
+    Sends text to the Watson NLP and returns the raw response text
+    """
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyze}}
+
+    response = requests.post(url, headers=headers, json=input_json)
+    
+    #convert to dictionary
+    response_dict = json.loads(response.text)
+    emotions = response_dict["emotionPredictions"][0]["emotion"]
+
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score, 
+            'joy': joy_score, 
+            'sadness': sadness_score, 
+            'dominant_emotion': dominant_emotion}
+    
